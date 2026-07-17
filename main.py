0dd3461105c2c891461e697f6bbfdc84d83ef327
Siti Rohimatuz Zahro <sukarami.1996@gmail.com>
2026-07-17 12:24:33 +0700

    API url middleware

--- main.py
+++ main.py
@@ -50,9 +50,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+    allow_origins = [
         "http://localhost:5173",
         "https://eczema-psoriasis-detection.vercel.app",
+        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
